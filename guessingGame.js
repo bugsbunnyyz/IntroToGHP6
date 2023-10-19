3b4a2dacbadcfa02e1e66d6c5b6614259d3952ae
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2023-10-18 19:07:56 -0700

    Add files via upload

---
+++ guessingGame.js
@@ -0,0 +1,10 @@
+function playerGuess(){
+    let guess = document.getElementById("guess1").value;
+    alert(guess);
+}
+Var guessNumA = document.getElementById("guess1");
+undefined
+guessNumA.value
+'55'
+guessNumA.placeholder = "Hello";
+'Hello'
